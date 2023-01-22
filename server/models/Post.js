15bbab847be5bddd4a72e0259537ea20c5abde60
Nikhil <nikhilc71@gmail.com>
2023-01-23 00:22:08 +0530

    s

---
+++ server/models/Post.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    likes: {
+        type: Map,
+        of: Boolean,
+    }
+})
