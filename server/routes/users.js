de7bb3b016c885b630d8d997cae1ff3523733f2a
Nikhil <nikhilc71@gmail.com>
2023-06-14 19:49:44 +0530

    reviewing for functionality

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,19 @@
-import express from 'express';
-import { 
-    getUser, 
-    getUserFriends, 
-    addRemoveFriend,
+import express from "express";
+import {
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
 } from "../controllers/users.js";
-import { verifyToken } from '../middleware/auth.js';
+import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
-// READ
-router.get('/:id', verifyToken, getUser);
-router.get('/:id/friends', verifyToken, getUserFriends);
+/* READ */
 
-// UPDATE
-router.patch('/:id/:friendId', verifyToken, addRemoveFriend);
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
